1684650787e1c41440dc6884f3331a533cd282f1
kishore <karanamkishorekvk@gmail.com>
2023-05-09 22:53:27 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+function SignupPage() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // TODO: Handle form submission
+  }
+
+  return (
+    <div>
+      <h2>Sign up</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignupPage;
